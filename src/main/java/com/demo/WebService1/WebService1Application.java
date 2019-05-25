4b9a1c5c07eac65f7a6b2ef933748415f3d71387
Bala <bala4ru@gmail.com>
2019-05-24 20:10:18 -0400

    initial

---
+++ src/main/java/com/demo/WebService1/WebService1Application.java
@@ -0,0 +1,21 @@
+package com.demo.WebService1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class WebService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebService1Application.class, args);
+	}
+
+	@RequestMapping(value="demo")
+	public String demo(){
+		
+		return "Demo Welcomes you...............";
+	}
+}
