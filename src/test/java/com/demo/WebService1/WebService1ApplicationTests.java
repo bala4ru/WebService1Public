aadc9536cf763f3779ffde883282da8e50ddf55a
Bala <bala4ru@gmail.com>
2019-05-26 13:02:26 -0400

    added junit test case

--- src/test/java/com/demo/WebService1/WebService1ApplicationTests.java
+++ src/test/java/com/demo/WebService1/WebService1ApplicationTests.java
@@ -1,9 +1,12 @@
 package com.demo.WebService1;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,4 +16,10 @@ public class WebService1ApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testDemo(){
+		WebService1Application app = new WebService1Application();
+		String s = app.demo();
+		assertEquals(true,s.contains("Demo"));
+	}
 }
