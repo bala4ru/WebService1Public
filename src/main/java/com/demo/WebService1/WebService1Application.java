525ec140adf9a4b4a284073c3df6c0b488468141
Bala <bala4ru@gmail.com>
2019-06-13 17:00:51 -0400

    dummy commit

--- src/main/java/com/demo/WebService1/WebService1Application.java
+++ src/main/java/com/demo/WebService1/WebService1Application.java
@@ -16,7 +16,7 @@ public class WebService1Application {
 	@RequestMapping(value="demo")
 	public String demo(){
 		//dummy updated..
-		//TODO
+		
 		return "Demo Welcomes you...............";
 	}
 }
